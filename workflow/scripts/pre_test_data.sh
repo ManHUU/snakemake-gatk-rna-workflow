a5bd0da1e72861f6bdd614106fc5df9c2812d9b4
Man Hu <mhu@int5.local.snellius.surf.nl>
2026-01-04 18:57:38 +0100

    Add test data and delete unrelated code

--- workflow/scripts/pre_test_data.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p ./test_fastqs
-
-# Subset the first 400,000 lines (100k reads) of each FASTQ
-# Note: Update the path '/path/to/your/liver_fastq/' to the real location of your files
-for file in /projects/prjs1616/gatk_paper_demo/data/liver_fastq/*.fastq; do
-    filename=$(basename "$file")
-    echo "Subsetting $filename..."
-    head -n 400000 "$file" > "./test_fastqs/$filename"
-done
